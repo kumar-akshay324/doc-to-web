1ddc87e63af7cb1d205bcca7238c9275f16d37c3
kumar-akshay324 <2013uee1340@mnit.ac.in>
2018-09-18 22:27:29 -0700

    added dictionary for tags

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# doc-to-web
-A Python 3 based side-kick project that, processes a normal text file and based on special tags analogous to Markdown, creates content in a provided HTML webpage  
+## Text Document to HTML Webpage
+
+A Python 3 based side-kick project that, processes a normal text file and based on special tags analogous to Markdown,
+either creates content in a provided HTML webpage OR creates HTML format for the same
+
+## Annotations in Text Document to HTML tags
+
+* >         : <h1>
+* >>        : <h2>
+* >>        : <h3>
+
+* -         : <ul>
+
+* Text      : <p>
+
+* ----      : <div>
+
+* ____      : <br>
+
+* _abc_     : <i>
+
+* **abc**   : <b>
+
--- doc_process.py
+++ doc_process.py
@@ -4,27 +4,40 @@ import csv
 
 class DocToWeb():
 
-
     def __init__(self, file_n, line_pointer):
         self.line_pointer = line_pointer
         self.file_name = file_n
+        self.tagDictionary()
         print ("Processing the document")
 
     def parse_text(self):
 
         with open (self.file_name, 'r') as text_file:
-
             for index, line in enumerate(text_file):
                 if (index+1) >= self.line_pointer:
                     line_word_list = line.split()
                     tagType = self.checkTagType(line_word_list[0])
 
-
-
     def checkTagType(self, symbol):
 
-        self.tagDict[symbol]
+        for index, dictnry in enumerate(self.tagDict_list):
+            try:
+                return dictnry[symbol]
+            except:
+                continue
 
     def tagDictionary(self):
 
-        self.tagDict = {">>>":"p", "*":"b", }
+        self.tagDict_inline = {">": ["<h1>", "</h1>"], ">>": ["<h2>", "</h2>"], ">>>": ["<h3>", "</h3>"]}
+        self.tagDict_para   = {"____": "<br>", "-": "<ul>", "----": ["<div>", "</div>"]}
+        self.tagDict_word   = {"_abc_": ["<i>", "</i>"], "*abc*": ["<b>", "</b>"]}
+
+        self.tagDict_list = [self.tagDict_para, self.tagDict_inline, self.tagDict_word]
+
+    def checkBold(self, stra, strb):
+        if (str == "*") and (strb=="*"):
+            return True
+
+    def checkItalics(self, stra, strb):
+        if (str == "_") and (strb=="_"):
+            return True
